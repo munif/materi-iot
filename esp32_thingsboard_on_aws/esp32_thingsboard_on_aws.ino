c433142531966ee9810ed880149abd8b56f1ceb8
Abdul Munif <munif.if@gmail.com>
2019-08-13 15:17:52 +0700

    Adding esp32 for AWS server

---
+++ esp32_thingsboard_on_aws/esp32_thingsboard_on_aws.ino
@@ -0,0 +1,57 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+const char* ssid = "S7-5695";
+const char* password =  "password123";
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(4000);   //Delay needed before calling the WiFi.begin
+ 
+  WiFi.begin(ssid, password); 
+ 
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+}
+
+void loop() {
+  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+    HTTPClient http;
+    http.begin("http://18.221.254.87:8080/api/v1/kuPyFpHiiMXOecZOuTwX/telemetry");
+    
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.println("Sending requests");
+
+    // Generate random temperature
+    long randNumber = random(10);
+    double randTemp = randNumber * 0.5 + 25.0;
+    Serial.println("Random temperature: " + String(randTemp));
+
+    // Send POST data
+    int httpResponseCode = http.POST("{\"temperature\": " + String(randTemp) + "}");   //Send the actual POST request
+    
+    if(httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.print(httpResponseCode);
+    }
+    
+    http.end();
+  }
+  else
+  {
+      Serial.println("Error in WiFi connection");   
+  }
+ 
+  delay(3000);  //Send a request every 3 seconds
+
+}
